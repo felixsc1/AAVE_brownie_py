edb43032c9b7afc1b2326134d5b072a38848f058
Felix Schlegel <schlegel.felix@gmail.com>
2021-12-07 14:42:49 +0100

    Added approve function for ERC20 tokens, prerequisite for step2

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,6 +1,10 @@
 from brownie import config, network, interface
 from scripts.helpful_scripts import get_account, FORKED_LOCAL_ENVIRONMENTS
 from scripts.get_weth import get_weth
+from web3 import Web3
+
+# amount to allow during verify step:
+amount = Web3.toWei(0.1, "ether")
 
 
 def main():
@@ -9,7 +13,16 @@ def main():
     if network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
         get_weth(0.1)
     lending_pool = get_lending_pool()
-    print(lending_pool)
+    approve_erc20(amount, lending_pool.address, erc20_address, account)
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving ERC20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
+    return tx
 
 
 def get_lending_pool():
